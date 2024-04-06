45c66b721db65429368a3afa5f36f2f1f8032c3a
Abhishekfpaipl <abhishekfpaipl@gmail.com>
2024-04-06 18:19:00 +0530

    changed api url

--- src/modules/macHiddle/axiosInstance.js
+++ src/modules/macHiddle/axiosInstance.js
@@ -3,8 +3,8 @@
 import axios from 'axios';
 // const token = localStorage.getItem('token');
 const axiosInstance = axios.create({
-    // baseURL: 'https://server.machiddle.com/api/', // Your base URL
-    baseURL: 'http://192.168.1.133:8000/api/', // Your base URL
+    baseURL: 'https://server.machiddle.com/api/', // Your base URL
+    // baseURL: 'http://192.168.1.133:8000/api/', // Your base URL
     // headers: {
     //     "Authorization": `Bearer ${token}`
     // }
